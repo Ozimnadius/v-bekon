5a739881313028e9c418e7f90906eba12480eed0
Mikle Seregin <mikhail-seregin@yandex.ru>
2022-06-09 17:23:14 +0300

    finish

--- js/script.js
+++ js/script.js
@@ -42,7 +42,7 @@ const media = {
 
 window.addEventListener("load", function () {
 
-    $('.input[type="tel"]').inputmask("+7(999)999-99-99");
+    $('input[type="tel"]').inputmask("+7(999)999-99-99");
 
 });
 
--- src/default/js/common.js
+++ src/default/js/common.js
@@ -37,6 +37,6 @@ const media = {
 
 window.addEventListener("load", function () {
 
-    $('.input[type="tel"]').inputmask("+7(999)999-99-99");
+    $('input[type="tel"]').inputmask("+7(999)999-99-99");
 
 });
